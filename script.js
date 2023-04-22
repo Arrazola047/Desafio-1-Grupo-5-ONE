34b00df1c2e3dd71ee4a4c032dd196e890f521bc
Ramon Arrazola <arrazola047@gmail.com>
2023-04-21 19:58:51 -0600

    Se crearon los dos div's principales

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var textoentrada= document.querySelector("cuadro");
+var textoentrada= document.getElementById("cuadro").value;
 
 function encriptar(encriptado){
-    var texto = textoentrada.value;
-    console.log(texto);
-    var encriptado = texto.replace(/ai/img,"a")
-    var encriptado = texto.replaceAll("a","ai");
+    console.log(textoentrada);
+    var encriptado = textoentrada.replace(/ai/img,"a")
+    encriptado = textoentrada.replaceAll("a","ai");
     console.log(encriptado);
 
     document.getElementById("resuelto").innerHTML = encriptado;
@@ -13,7 +12,7 @@ function encriptar(encriptado){
 function desencriptar(){
     var texto = document.getElementById("input").value.toLowerCase();
 
-    var desencriptado = texto.replace(/ai/img,"a");
+    var desencriptado = texto.replace(/ai/img,"a"); 
     var desencriptado = desencriptado.replace(/enter/img,"e");
     var desencriptado = desencriptado.replace(/imes/img,"i");
     var desencriptado = desencriptado.replace(/ober/img,"o");
